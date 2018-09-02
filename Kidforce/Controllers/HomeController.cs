ea266ac6a768b5fb36f8b216aa15172310c572c9
MichaelHeimburger <mrheimburger45@gmail.com>
2018-09-01 22:35:24 -0400

    Tossing around layout ideas

--- Kidforce/Controllers/HomeController.cs
+++ Kidforce/Controllers/HomeController.cs
@@ -16,15 +16,17 @@ namespace Kidforce.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
+        //public ActionResult Login()
+        //{
+
+        //}
     }
 }
